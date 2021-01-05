037ffa63299c8228953a8bed3c8a6cc8e7a3b00e
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2021-01-05 18:16:28 -0300

    Domain model

--- backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-
+ 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
